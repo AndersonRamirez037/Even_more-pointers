9b0fbc7bd01de943796e66ed8ad5df9a902f8d24
AndersonRamirez037 <andersongamer037@gmail.com>
2023-05-08 15:40:01 -0500

    5. strstr

---
+++ 5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/*
+ * _strstr - function that locates a substring. 
+ * @char, char 
+ * return char or NULL.
+*/
+char *_strstr(char *haystack, char *needle){
+    if(*needle == '\0'){
+        return haystack; 
+    }
+    while(*haystack != '\0'){
+        char *copy_hay = haystack;
+        char *copy_need = needle;
+        while (*copy_hay != '\0' && *copy_need != '\0' && *copy_hay == *copy_need){
+            copy_hay++;
+            copy_need++;
+        }
+        if(*copy_need == '\0'){
+            return haystack; 
+        }
+        haystack++; 
+    }
+    return NULL;
+}
