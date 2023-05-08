f475c07614ec578455a542cea05d752950dc7173
AndersonRamirez037 <andersongamer037@gmail.com>
2023-05-07 19:52:44 -0500

    2. strchr

---
+++ 2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stddef.h>
+/*
+ * _strchr - function that locates a character in a string. 
+ * @char, char
+ * return char.
+*/
+char *_strchr(char *s, char c){
+    while(*s != '\0'){
+        if(*s == c){
+            return s; 
+        }
+        s++;
+    }
+    return NULL;
+}
